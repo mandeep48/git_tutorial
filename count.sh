57924e494f0091eb6a50369c9c356fd6f0a54ede
Anish Talwar <anishtalwar@Rajivs-MBP.wireless.concordia.ca>
2016-01-09 14:04:08 -0500

    anish

--- count.sh
+++ count.sh
@@ -1,5 +1,5 @@
 # Display the name of each data file and count
-# the number of distinct occurrences of each species
+#the number of distinct occurrences of each species
 # in that file
 for filename in data/*.txt
 do
